33ca978ff6206441d237f15ded5c7f7e5705aec6
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2023-11-03 09:36:18 +0900

    ベースキャラクターの追加

---
+++ Code/Charactor/Charactor.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "OriginMath.h"
+
+class Charactor { //ゲーム内に登場する最低限のキャラクター、各種キャラはこれを継承して登場させてほしい
+	Vector3 pos;
+public:
+	Charactor() :pos(Vector3()) {}
+	virtual ~Charactor() {}
+
+	virtual void Update() {}
+	virtual void Draw() {}
+
+	const Vector3& ReadPosition() const { return pos; } //読み出し専用でポジションの取得
+	Vector3& EditPosition() { return pos; } //編集可能な形式でポジションの取得
+};
