adb37993213542ef5be02a4b36b646000ffe6dce
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2023-11-03 11:28:35 +0900

    スコア画面の処理

--- Code/Scene/Score.cpp
+++ Code/Scene/Score.cpp
@@ -1,9 +1,35 @@
 #include "DxLib.h"
 #include "Score.h"
+#include"ConstVal.h"
 
-Score::Score(int score)
+Score::Score(int now_score)
 {
-	this->score = score;
+	this->now_score = now_score;
+
+	//FILE* fp_s;//ランキングの読み込み
+	//fopen_s(&fp_t, treasure_data[stage_num], "r");
+
+	//for (int i = 0; i < STAGE_BLOCK_NUM_Y; i++)
+	//{
+	//	for (int j = 0; j < stage_width; j++)
+	//	{
+	//		int block_type, treasure_type;
+	//		fscanf_s(fp_s, "%d", &block_type);
+	//		fscanf_s(fp_t, "%d", &treasure_type);
+
+	//		DATA location;
+	//		location.x = ((j - 1) * BLOCK_SIZE_X) + (BLOCK_SIZE_X / 2);
+	//		location.y = (i * BLOCK_SIZE_Y) + (BLOCK_SIZE_Y / 2);
+
+	//		if (block_type != -1)block.emplace_back(location, block_type);
+	//		if (treasure_type == 5) flag_location = location;
+	//		else if ((treasure_type != -1) && treasure_type != 0)treasure.emplace_back(location, treasure_type);
+	//	}
+	//}
+
+	//fclose(fp_s);
+	//fclose(fp_t);
+
 }
 
 void Score::Update()
@@ -13,5 +39,5 @@ void Score::Update()
 
 void Score::Draw()
 {
-	DrawFormatString(100, 100, 0xffffff, "スコア　%d", score);
+	DrawFormatString(WINDOW_X / 2, WINDOW_Y / 2, 0xffffff, "スコア　%d", now_score);
 }
--- Code/Scene/Score.h
+++ Code/Scene/Score.h
@@ -4,10 +4,11 @@
 class Score :public Scene
 {
 public:
-	Score(int score);
+	Score(int now_score);
 	void Update()override; //毎フレーム実行する処理関数、実装必須
 	void Draw()override; //毎フレーム実行する描写関数
 private:
-	int score;
+	int now_score;
+	int ranking_score[5];
 };
 
