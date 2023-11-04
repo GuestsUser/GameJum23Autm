ed9865d088d3c66003f4756667c347a870796b47
Nakora-koutyan <104492122+Nakora-koutyan@users.noreply.github.com>
2023-11-04 13:38:04 +0900

    弾のCollisionの追加

--- Code/Charactor/Shoot.h
+++ Code/Charactor/Shoot.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Charactor.h"
 #include "DxLib.h"
+#include "Collision.h"
 
 #define CAMERA_CENTER_X 480
 class Shoot :public Charactor
@@ -21,6 +22,7 @@ private:
 	int shoot_hit_red;	//赤の弾のヒット画像
 
 	Color color;		//弾の色の識別
+	Collision* ball_collision;
 
 public:
 	Shoot();	//コンストラクタ
