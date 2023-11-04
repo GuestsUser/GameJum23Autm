dfbc6faaacdd9a30a06cf8fa975f955b8aeef23e
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-11-04 15:28:13 +0900

    画面全体がちらつくのを解決した多分
    弾が消える時に弾全体がちらつくのは治っていない。
    
    http://studiokingyo.fc2web.com/dxlib/kaibou/trouble.html#:~:text=%E6%A6%82%E8%A6%81%3A,%E3%81%99%E3%82%8C%E3%81%B0%E3%81%99%E3%81%B9%E3%81%A6%E8%A7%A3%E6%B1%BA%E3%81%A7%E3%81%99%E3%80%82

--- Code/main.cpp
+++ Code/main.cpp
@@ -25,10 +25,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetWindowText("1ボタン"); //画面タイトル設定
 	SetGraphMode(WINDOW_X, WINDOW_Y, 32); //画面モードの設定
 	SetBackgroundColor(0, 0, 0); //画面の背景色の設定
-	SetDrawScreen(DX_SCREEN_BACK);
 
 	if (DxLib_Init() == -1) { return -1; }		//初期化と異常が認められた場合の終了
 
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	SoundManager::GetInstance();
 	WorldVal::SetUp();
