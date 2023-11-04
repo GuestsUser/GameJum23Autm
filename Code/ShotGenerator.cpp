7d7bdc02189234f0b05ad1a0f84da7f2577bacda
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2023-11-04 15:01:52 +0900

    生成別パターン試作

--- Code/ShotGenerator.cpp
+++ Code/ShotGenerator.cpp
@@ -5,11 +5,16 @@
 #include "SceneAccessor.h"
 #include "Shoot.h"
 
-#include "ConstVal.h"
 
 ShotGenerator::ShotGenerator() :count(0), spawTime(60), highLevelSpan(6) {
 	baseSpeed = WINDOW_X / 2 / 150; //画面端から画面中央まで150フレームで到達するスピード
 	edit = SceneAccessor::GetInstance();
+
+	current_pattern = GetRand(4);
+	left_upper = 4;
+	left_footer = 4;
+	right_upper = 5;
+	right_footer = 5;
 }
 
 void ShotGenerator::Update() {
@@ -20,16 +25,31 @@ void ShotGenerator::Update() {
 	}
 
 	//ここまで来れればスポーンタイミングになっている
-	Place placeMode = (Place)GetRand((int)Place::len - 1); //配置形式の作成
-	Type typeMode = Type::normal;
+	//Place placeMode = (Place)GetRand((int)Place::len - 1); //配置形式の作成
+	//Type typeMode = Type::normal;
 
-	if ((count / spawTime) % highLevelSpan == 0) { typeMode = (Type)GetRand((int)Type::len - 1); } //ハイレベルのタイミングであればType抽選
+	//if ((count / spawTime) % highLevelSpan == 0) { typeMode = (Type)GetRand((int)Type::len - 1); } //ハイレベルのタイミングであればType抽選
 
-	if (placeMode == Place::normal) {
-		ShotGenerat(Place::left, typeMode);
-		ShotGenerat(Place::right, typeMode);
+	//if (placeMode == Place::normal) {
+	//	ShotGenerat(Place::left, typeMode);
+	//	ShotGenerat(Place::right, typeMode);
+	//}
+	//else { ShotGenerat(placeMode, typeMode); }
+
+	GenerationBasedOnPattern();
+	left_upper--;
+	left_footer--;
+	right_upper++;
+	right_footer++;
+	
+	if (left_upper < 0)
+	{
+		current_pattern = GetRand(4);
+		left_upper = 4;
+		left_footer = 4;
+		right_upper = 5;
+		right_footer = 5;
 	}
-	else { ShotGenerat(placeMode, typeMode); }
 }
 
 void ShotGenerator::ShotGenerat(Place place, Type type) { //ここに書いている数値は仮の物
@@ -43,13 +63,43 @@ void ShotGenerator::ShotGenerat(Place place, Type type) { //ここに書いている数値
 	if (type == Type::lowSpeed) { speed = 0.5; }
 
 	position.SetX(-32);
-	if (place == Place::right) { 
+	if (place == Place::right) {
 		position.SetX(WINDOW_X + 32);
 		speed *= -1;
 	}
 
-	Shoot* shot=edit->GetCurrentScene()->CreateObject<Shoot>(position);
+	Shoot* shot = edit->GetCurrentScene()->CreateObject<Shoot>(position);
 
 	shot->SetSpeed(baseSpeed * speed);
 	shot->SetBoxColor(Color(GetRand(1)));
+}
+
+void ShotGenerator::GenerationBasedOnPattern()
+{
+	Vector3 position;
+	Shoot* shot;
+
+	//	左上
+	position = Vector3(LEFT, UP, 0);
+	shot = edit->GetCurrentScene()->CreateObject<Shoot>(position);
+	shot->SetBoxColor(Color(generation_patterns[current_pattern][0][left_upper]));
+	shot->SetSpeed(baseSpeed);
+
+	//	右上
+	position = Vector3(RIGHT, UP, 0);
+	shot = edit->GetCurrentScene()->CreateObject<Shoot>(position);
+	shot->SetBoxColor(Color(generation_patterns[current_pattern][0][right_upper]));
+	shot->SetSpeed(-baseSpeed);
+
+	//	左下
+	position = Vector3(LEFT, FOOT, 0);
+	shot = edit->GetCurrentScene()->CreateObject<Shoot>(position);
+	shot->SetBoxColor(Color(generation_patterns[current_pattern][1][left_footer]));
+	shot->SetSpeed(baseSpeed);
+
+	//	右下
+	position = Vector3(RIGHT, FOOT, 0);
+	shot = edit->GetCurrentScene()->CreateObject<Shoot>(position);
+	shot->SetBoxColor(Color(generation_patterns[current_pattern][1][right_footer]));
+	shot->SetSpeed(-baseSpeed);
 }
--- Code/ShotGenerator.h
+++ Code/ShotGenerator.h
@@ -1,9 +1,40 @@
 #pragma once
 #include "Charactor.h"
+#include "ConstVal.h"
+#define LEFT (0 - 32)
+#define RIGHT (WINDOW_X + 32)
+#define UP	 (360)
+#define FOOT (430)
 
 class SceneAccessor;
 
+
 class ShotGenerator :public Charactor {
+
+	const int generation_patterns[5][2][10] =
+	{
+		{
+			{9,0,9,9,9,9,9,9,0,9},
+			{9,0,1,1,9,9,1,1,0,9}
+		},
+		{
+			{9,1,1,9,9,9,9,1,0,9},
+			{9,9,1,0,1,1,0,1,9,9}
+		},
+		{
+			{0,9,9,9,1,0,9,9,9,1},
+			{9,0,0,1,0,1,0,1,1,9}
+		},
+		{
+			{9,9,9,9,9,9,9,9,9,9},
+			{9,0,0,0,9,9,1,0,1,0}
+		},
+		{
+			{9,9,9,1,9,9,0,9,9,9},
+			{1,0,9,1,9,9,0,9,1,0}
+		},
+	};
+
 	enum class Place { normal, left, right, len }; //配置の形式、normalなら左右に、left、rightならそちらのみに
 	enum class Type { normal, up, highSpeed, lowSpeed, len }; //normalなら通常の高さ、upで高い位置、speed系は動作速度が変化する
 
@@ -15,6 +46,14 @@ class ShotGenerator :public Charactor {
 	SceneAccessor* edit;
 
 	void ShotGenerat(Place place, Type type);
+
+
+	void GenerationBasedOnPattern();
+	int current_pattern;
+	int left_upper;
+	int right_upper;
+	int left_footer;
+	int right_footer;
 
 public:
 	ShotGenerator();
